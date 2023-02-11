6e9619df254b94eb36b1ebed2194b7ac03f6548b
Daria <darya.piankova@yandex.ru>
2023-02-11 17:16:53 +0500

    corrected task27

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число A");
-int a= Convert.ToInt32(Console.ReadLine());
-int sum=0;
-for(;a!=0;a=a/10){
-sum=sum+a%10;
+﻿void clear(){
+    Console.Clear();
 }
-Console.WriteLine(sum);
+
+int Prompt(string message){
+    Console.WriteLine(message);
+    int a=Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int sumDigit(int number){
+    int sum=0;
+    for(;number!=0;number=number/10){
+        sum=sum+number%10;
+    }
+    return sum;
+}
+
+clear();
+int number= Prompt("Введите число");
+Console.WriteLine($"Сумма цифр в числе {number} равна {sumDigit(number)}");
